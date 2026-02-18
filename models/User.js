cbeb51646d2b0f8e5c0cfa41f2e1dc9875a1b686
ricky oganyiaeze <rickyoganyiaeze@gmail.com>
2026-02-19 00:34:20 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -68,7 +68,7 @@ const UserSchema = new mongoose.Schema({
   },
   isOnline: {
     type: Boolean,
-    default: false
+    default: true
   },
   // This is the Admin field added correctly
   isAdmin: {
